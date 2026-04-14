ef62813f7a2ba3459f577ce67680bd8a4da9fd84
John Becerra <cardonabecerrajonathanmanuel@gmail.com>
2026-04-14 14:43:03 -0600

    Quitar campo de formulario de active campaing

--- components/ActiveCampaignForm.jsx
+++ components/ActiveCampaignForm.jsx
@@ -1,16 +1,15 @@
-'use client';
+"use client";
 
-import Script from 'next/script';
+import Script from "next/script";
 
 export default function ActiveCampaignForm() {
   return (
     <div className="ac-form-wrapper">
-      <div className="_form_179"></div>
-
-      <Script
+      <div class="_form_179"></div>
+      <script
         src="https://cefincapacitacion.activehosted.com/f/embed.php?id=179"
-        strategy="afterInteractive"
-      />
+        charset="utf-8"
+      ></script>
     </div>
   );
 }
